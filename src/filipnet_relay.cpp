8ce122565c2e2345651778f85919be3e4d6156df
filipnet <benedikt.filip@filipnet.de>
2022-06-19 21:00:59 +0200

    Library update

--- src/filipnet_relay.cpp
+++ src/filipnet_relay.cpp
@@ -1,1 +1,15 @@
 #include "filipnet_relay.h"
+
+Relay::Relay(const int pin, char mqtt){
+    pinMode(pin, OUTPUT);
+    _pin = pin;
+    _mqtt = mqtt;
+}
+
+void Relay::ON(){
+    digitalWrite(_pin, HIGH);
+}
+
+void Relay::OFF(){
+    digitalWrite(_pin, LOW);
+}
--- src/filipnet_relay.h
+++ src/filipnet_relay.h
@@ -1,9 +1,15 @@
 #ifndef FILIPNET_RELAY_H
 #define FILIPNET_RELAY_H
 #include <Arduino.h>
+
 class Relay {
-    public:
-        Relay(int pin);
     private:
+        int _pin;
+        char _mqtt;
+    public:
+        Relay(int pin, char mqtt);
+        void ON();
+        void OFF();
 };
+
 #endif
